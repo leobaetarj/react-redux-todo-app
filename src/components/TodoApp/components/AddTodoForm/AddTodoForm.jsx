706f251f76ffe2f2f9af426bd72c2b2ced97a3cb
Leo Baeta <leobaetarj@gmail.com>
2019-04-13 17:15:44 -0300

    Refact onClick handler in AddTodoForm

--- src/components/TodoApp/components/AddTodoForm/AddTodoForm.jsx
+++ src/components/TodoApp/components/AddTodoForm/AddTodoForm.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const AddTodoForm = ({ onAddTodo }) => {
-  let input;
+  const textInput = React.createRef();
+
+  const clearText = () => {
+    textInput.current.value = '';
+  };
+
+  const handleClick = () => {
+    const { value } = textInput.current;
+    if (value !== '') {
+      onAddTodo(value);
+      clearText();
+    }
+  };
+
   return (
     <>
-      <input
-        name="addTodo"
-        ref={(node) => {
-          input = node;
-        }}
-      />
-      <button
-        type="button"
-        onClick={() => {
-          onAddTodo(input.value);
-          input.value = '';
-        }}
-      >
+      <input name="addTodo" ref={textInput} />
+      <button type="button" onClick={handleClick}>
         Add
       </button>
     </>
