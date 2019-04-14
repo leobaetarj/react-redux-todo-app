bc6d2a0797be322d64839c671d211fa05864ceae
Leo Baeta <leobaetarj@gmail.com>
2019-04-14 16:50:11 -0300

    Add Filter to TodoApp Component

--- src/components/TodoApp/TodoApp.jsx
+++ src/components/TodoApp/TodoApp.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { AddTodoForm } from '../../containers/AddTodoForm/AddTodoForm';
 import { TodoList } from '../../containers/TodoList/TodoList';
+import { Filter } from '../../containers/Filter/Filter';
 
 export const TodoApp = () => (
   <div className="todo-app">
     <h1>TodoApp</h1>
     <AddTodoForm />
     <TodoList />
+    <Filter />
   </div>
 );
