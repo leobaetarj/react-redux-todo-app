871f8135c42b6935b00d188362914faf5405cf55
Leo Baeta <leobaetarj@gmail.com>
2019-04-14 16:50:38 -0300

    Add filter in TodoList mapStateToProps

--- src/containers/TodoList/TodoList.js
+++ src/containers/TodoList/TodoList.js
@@ -1,8 +1,17 @@
 import { connect } from 'react-redux';
 import { TodoList as Component } from '../../components/TodoApp/components/TodoList/TodoList';
 import { toogleTodo } from '../../actions/actionCreators';
+import { SHOW_COMPLETED } from '../../components/TodoApp/components/Filter/Filter';
 
-const mapStateToProps = state => state;
+const mapStateToProps = (state) => {
+  if (state.visibilityFilter === SHOW_COMPLETED) {
+    return {
+      ...state,
+      todos: state.todos.filter(todo => todo.completed),
+    };
+  }
+  return state;
+};
 const mapDispatchToProps = dispatch => ({
   onToogleTodo: id => dispatch(toogleTodo(id)),
 });
