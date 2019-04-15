cc071cadada7670b82ea2be7947dfa6d6b0d3695
Leo Baeta <leobaetarj@gmail.com>
2019-04-15 08:43:56 -0300

    Add TodoList selector to get Todos correctly

--- src/containers/TodoList/TodoList.js
+++ src/containers/TodoList/TodoList.js
@@ -1,17 +1,27 @@
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+
 import { TodoList as Component } from '../../components/TodoApp/components/TodoList/TodoList';
 import { toogleTodo } from '../../actions/actionCreators';
 import { SHOW_COMPLETED } from '../../components/TodoApp/components/Filter/Filter';
 
-const mapStateToProps = (state) => {
-  if (state.visibilityFilter === SHOW_COMPLETED) {
-    return {
-      ...state,
-      todos: state.todos.filter(todo => todo.completed),
-    };
-  }
-  return state;
-};
+const getStateTodos = state => state.todos;
+const getStateVisibilityFilter = state => state.visibilityFilter;
+
+const getTodos = createSelector(
+  [getStateTodos, getStateVisibilityFilter],
+  (todos, visibilityFilter) => {
+    if (visibilityFilter === SHOW_COMPLETED) {
+      return todos.filter(todo => todo.completed);
+    }
+    return todos;
+  },
+);
+
+const mapStateToProps = state => ({
+  ...state,
+  todos: getTodos(state),
+});
 const mapDispatchToProps = dispatch => ({
   onToogleTodo: id => dispatch(toogleTodo(id)),
 });
